775bc34e6bae3dcab3b521bdede6c731599b2894
UltraFrank <Francescobotta02@gmail.com>
2024-12-26 18:14:30 +0100

    A

--- Dashing_Cube/Assets/Scenes/Scripts/SessionScripts/PlatformController.cs
+++ Dashing_Cube/Assets/Scenes/Scripts/SessionScripts/PlatformController.cs
@@ -18,6 +18,11 @@ public class PlatformController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Init();
+    }
+
+    public void Init()
+    {
         speed = 0;
         StartCoroutine(GameStarting());
     }
